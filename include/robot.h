96326f09bd4829f5b1680f333009b9833e6dbd3d
Luc Varoqui <luc@varoqui.org>
2019-05-14 11:55:00 +0200

    Basic robot movements

--- include/robot.h
+++ include/robot.h
@@ -5,16 +5,34 @@
 #ifndef ETUDE_ROBOT_H
 #define ETUDE_ROBOT_H
 
+#include <iostream>
+#include <cmath>
 
 class Robot {
-    float x_;
-    float y_;
-    float direction_;
+    double x_;
+    double y_;
+    double direction_;
 
 public:
-    Robot(float X, float Y, float Direction): x_(X), y_(Y), direction_(Direction) {}
+    /// Constructor of Robot class
+    /// \param X Initial X position
+    /// \param Y Initial Y position
+    /// \param Direction Initial Direction
+    Robot(double X, double Y, double Direction): x_(X), y_(Y), direction_(Direction) {}
 
+    /// Move the robot forward
+    /// \param distance Distance to travel
+    void moveForward(double distance);
+
+    /// Rotate the robot
+    /// \param angle Angle to rotate (gradient)
+    void rotate(double angle);
+
+    /// Display information about the robot to output stream
+    /// \param os Output Stream
+    /// \param robot The robot
+    /// \return New Output Stream
+    friend std::ostream& operator<<(std::ostream& os, const Robot& robot);
 };
-
 
 #endif //ETUDE_ROBOT_H
